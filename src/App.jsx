c4e6de535871b348ff7cbde665e36daab05b2b1a
David Faletto <davidfaletto@gmail.com>
2024-11-28 12:11:36 -0300

    modificando la plantilla

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// en javascript todos las herramientas que necesitemos usar
+// css
+// librerias
 
+
+
+function App() {
+  // codigo javascript y react 
+  //let productosEnCarrito = 3
+  /*let styles = {
+    margin: '10px',
+    backgroundColor: 'red',
+    fontSize: '20px',
+  }*/ 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <div className="logo">Mi Tienda</div>
+        <ul className="navbar-nav ml-auto" >
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              href="#"
+            >
+              Inicio
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Productos
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Contacto
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Carrito
+            </a>
+            {/* <span>
+              {" "}
+              {productosEnCarrito > 5
+                ? "Hay descuento"
+                : productosEnCarrito}{" "}
+            </span> */}
+          </li>
+        </ul>
+      </nav>
+
+      <section className="products">
+        <h1>Productos Destacados</h1>
+        <div className="product-grid">
+          <div className="product">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWj4_QrinB4Msvk2sRg60VWSdEySiGPmlylg&s"
+              alt="Producto 1"
+            />
+            <h2>Producto 1</h2>
+            <p>$10.00</p>
+            <button>Añadir al carrito</button>
+          </div>
+          <div className="product">
+            <img
+              src="https://d22fxaf9t8d39k.cloudfront.net/31177e5345ac3dfec93652de8031a5f5bdf755ec9f8224f8c61c4b7b7aeccee9104532.webp"
+              alt="Producto 2"
+            />
+            <h2>Producto 2</h2>
+            <p>$20.00</p>
+            <button>Añadir al carrito</button>
+          </div>
+          <div className="product">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjxXWTypp1bdoPyCYo7qdqyWvKj11OquEMGg&s"
+              alt="Producto 3"
+            />
+            <h2>Producto 3</h2>
+            <p>$30.00</p>
+            <button>Añadir al carrito</button>
+          </div>
+        </div>
+      </section>
+
+      <footer>
+        <p>&copy; 2024 Mi Tienda Online</p>
+      </footer>
     </>
-  )
+  );
 }
+
 
 export default App
